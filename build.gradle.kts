7778f4e31e623c5323a2e8c753e0655d394b1f8a
770grappenmaker <770grappenmaker@gmail.com>
2024-01-05 14:42:01 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.github.770grappenmaker:mappings-util:0.1.2")
+}
---
+++ src/main/kotlin/com/grappenmaker/mixin/MixinTesting.kt
@@ -0,0 +1,5 @@
+package com.grappenmaker.mixin
+
+fun main() {
+
+}
